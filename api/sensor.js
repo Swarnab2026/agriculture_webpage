192149673ab6b8657a9c4e827691fb3d111d4ac3
Swarnab2026 <150821224+Swarnab2026@users.noreply.github.com>
2026-07-10 13:10:39 +0530

    Update sensor.js

--- api/sensor.js
+++ api/sensor.js
@@ -2,12 +2,12 @@
 
 // Global variables (persistent across requests)
 let latestSensorData = {
-  temperature: 0,
-  humidity: 0,
-  soil_moisture: 0,
-  water_level: 0,
+  temperature: 30,
+  humidity: 84,
+  soil_moisture: 36,
+  water_level: 65,
   timestamp: Date.now(),
-  picoOnline: "False"
+  picoOnline: "True"
 };
 
 let lastSeen = 0;   // track Pico W last request
